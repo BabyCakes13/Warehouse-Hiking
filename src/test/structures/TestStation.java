8c281d1ceb45bef12093c1f811e156543521d49e
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-10-26 00:47:14 +0300

    Added test for getStationNumber and replaced iteration values with contants.

--- src/test/structures/TestStation.java
+++ src/test/structures/TestStation.java
@@ -1,12 +1,10 @@
 package test.structures;
 
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Assert;
-
 import structures.Station;
 
 public class TestStation {
@@ -25,7 +23,8 @@ public class TestStation {
 	public void testToStringGenerated() {
 		double range = (double) Integer.MAX_VALUE - Integer.MIN_VALUE + 1;
 
-		for (int i = 0; i < 1000; i++) {
+		final int stationsNumber = 1000;
+		for (int i = 0; i < stationsNumber; i++) {
 			int newRandom = (int) (Math.random() * range + Integer.MIN_VALUE);
 			Station newStation = new Station(newRandom);
 			Assert.assertEquals("Station " + newRandom, newStation.toString());
@@ -36,7 +35,7 @@ public class TestStation {
 	public void testHashCode() {
 		int[] testStationNumbers = { Integer.MIN_VALUE, 0, Integer.MAX_VALUE };
 		
-		final int testRange = 2;
+		final int testRange = 10;
 		for(int stationNumber : testStationNumbers) {
 			for (int i = -testRange ; i <= testRange ; i++) {
 				Station firstStation = new Station(stationNumber + i);
@@ -53,8 +52,14 @@ public class TestStation {
 		
 		Assert.assertEquals(firstStation, firstStation);
 		Assert.assertEquals(firstStation, secondStation);
-		Assert.assertNotEquals(1, firstStation);
+		
+		Assert.assertNotEquals(firstStation, 42);
+		
 		Assert.assertNotEquals(firstStation, null);
+		
+		Station thirdStation = new Station(2);
+		Assert.assertNotEquals(thirdStation, firstStation);
+
 	}
 	
 	@Test
@@ -63,17 +68,20 @@ public class TestStation {
 		ArrayList<Integer> testIntegers = new ArrayList<Integer>();
 		
 		int[] testStationNumbers = { Integer.MIN_VALUE, 0, Integer.MAX_VALUE };
-		final int testRange = 2;
+		final int testRange = 10;
 		for(int stationNumber: testStationNumbers) {
 			for(int i = -testRange; i <= testRange; i++) {
 				testStations.add(new Station(stationNumber + i));
 				testIntegers.add(stationNumber + i);
 			}
 		}
-		
 		Assert.assertEquals(testStations, Station.convertArrayToStation(testIntegers));
-		
-		
+	}
+	
+	@Test
+	public void testGetStationNumber() {
+		Station testStation = new Station(1);
+		Assert.assertEquals(1, testStation.getStationNumber());
 	}
 
 }
