c3d9e46b550f015843b90a2e99608f72aec204b1
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-10-26 00:51:51 +0300

    Refactored some code and finished with Station testing. Coverage 100%.

--- src/test/structures/TestStation.java
+++ src/test/structures/TestStation.java
@@ -2,7 +2,6 @@ package test.structures;
 
 import org.junit.Test;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.junit.Assert;
 import structures.Station;
@@ -34,54 +33,53 @@ public class TestStation {
 	@Test
 	public void testHashCode() {
 		int[] testStationNumbers = { Integer.MIN_VALUE, 0, Integer.MAX_VALUE };
-		
+
 		final int testRange = 10;
-		for(int stationNumber : testStationNumbers) {
-			for (int i = -testRange ; i <= testRange ; i++) {
+		for (int stationNumber : testStationNumbers) {
+			for (int i = -testRange; i <= testRange; i++) {
 				Station firstStation = new Station(stationNumber + i);
 				Station secondStation = new Station(stationNumber + i);
 				Assert.assertEquals(firstStation.hashCode(), secondStation.hashCode());
 			}
 		}
 	}
-	
+
 	@Test
 	public void testEqualsEqual() {
 		Station firstStation = new Station(1);
 		Station secondStation = new Station(1);
-		
+
 		Assert.assertEquals(firstStation, firstStation);
 		Assert.assertEquals(firstStation, secondStation);
-		
+
 		Assert.assertNotEquals(firstStation, 42);
-		
+
 		Assert.assertNotEquals(firstStation, null);
-		
+
 		Station thirdStation = new Station(2);
 		Assert.assertNotEquals(thirdStation, firstStation);
 
 	}
-	
+
 	@Test
 	public void testConvertArrayToStation() {
 		ArrayList<Station> testStations = new ArrayList<Station>();
 		ArrayList<Integer> testIntegers = new ArrayList<Integer>();
-		
+
 		int[] testStationNumbers = { Integer.MIN_VALUE, 0, Integer.MAX_VALUE };
 		final int testRange = 10;
-		for(int stationNumber: testStationNumbers) {
-			for(int i = -testRange; i <= testRange; i++) {
+		for (int stationNumber : testStationNumbers) {
+			for (int i = -testRange; i <= testRange; i++) {
 				testStations.add(new Station(stationNumber + i));
 				testIntegers.add(stationNumber + i);
 			}
 		}
 		Assert.assertEquals(testStations, Station.convertArrayToStation(testIntegers));
 	}
-	
+
 	@Test
 	public void testGetStationNumber() {
 		Station testStation = new Station(1);
 		Assert.assertEquals(1, testStation.getStationNumber());
 	}
-
 }
